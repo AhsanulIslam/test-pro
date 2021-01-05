86234ef662a7779aa0c84d388613d8f1453c76d1
Ahsanulislam <imon.ahsanul.islam@gmail.com>
2021-01-05 22:24:39 +0600

    fix bug

--- laratest/app/Http/Controllers/Auth/LoginController.php
+++ laratest/app/Http/Controllers/Auth/LoginController.php
@@ -66,7 +66,7 @@ class LoginController extends Controller
         $data ->password= bcrypt('123');
         $data->save();
  
-        return redirect('/loginindex')->with('status', 'Successfully sign Up!');;
+        return redirect('/loginindex')->with('status', 'Successfully sign Up!');
     }
 
     
